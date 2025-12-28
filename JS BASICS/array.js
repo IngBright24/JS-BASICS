325f671e32d9f6ebfdc0d75c0c048405db7ebce9
Ing. Bright Twumasi-Ankrah <twumasiankrahbright18@gmail.com>
2025-12-28 21:27:11 +0000

    Add files via upload

---
+++ JS BASICS/array.js
@@ -0,0 +1,14 @@
+//Write the JavaScript method used to remove the last item from an array.
+
+let cars = ["Toyota", "Honda", "Ford", "Chevrolet", "BMW"];
+let removedItem = cars.pop();
+console.log(cars);
+console.log(removedItem);
+
+
+
+//Write one method that adds an item to the beginning of an array.
+
+players = ["Suarez", "Maradona", "Lewandowski", "Lamine", "Neymar"];
+players.unshift("Messi(GOAT)"); 
+console.log(players);   
